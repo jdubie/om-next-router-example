8f79ef5092ffa75062d7570a0f20473c0afe2d05
Jack Dubie <jack.dubie@gmail.com>
2015-12-29 11:08:16 -0800

    Using total query approach

--- README.md
+++ README.md
@@ -1,19 +1,85 @@
 # Om Next Router
 
-A simple om next router that demonstrates client-side url-based routing,
-browser history.
+A simple om next router that demonstrates client-side url-based routing, browser history.
 
-The `Root` component listens for browser history events and syncs those to it's
-query and om state accordingly.
 
-The `Root` component listens for om state changes and syncs those to it's query
-and browser history.
-
-The `Root` component providing bi-directional syncing between om state and
-browser url is nice because now your parsing just deals with pure data and no
-browser / DOM manipulation.
-
+## Getting started
 ```
 git clone https://github.com/jdubie/om-next-router
 make
+# rlwrap lein run -m clojure.main script/figwheel.clj
+# figwheel is listening on http://localhost:4952
 ```
+
+## Routing approach
+
+Uses the **total query** paradigm. This means parsing a subset of the component's 
+total query. This subset is a function of what route is selected. One alternative to **total 
+query** is to update the root component's query via `om/set-query!`. I haven't figured out a 
+clean way to use `om/set-query!`.
+
+Nice properties of om:
+
+1. UI is a pure function of the global state atom
+1. Components are dumb and don't have logic - just queries and simple transact calls
+
+Let's flesh out of differences between these two approaches with an example and measure them 
+against our "nice properties of om".
+
+Let's say you have a button when a user clicks it you update the route based on some logic. 
+
+### total routing
+```
+;; ui
+(defui Component
+  static om/IQuery
+  (query [this]
+    [:foo/bar])
+  Object
+  (render [this]
+    (dom/button #js {:onClick (om/transact `[(button/clicked) :route])} "click me!"))
+
+;; parsing
+(defmethod mutate 'button/clicked
+  [{:keys [state]} _ _]
+  (if (valid? @state)
+    {:action #(swap! state merge {:route/current :foo :something :else})}
+    {:action #(swap! state merge {:route/current :bar :another :thing})}))
+```
+
+This is pretty clean. Not lets try using `om/set-query!`
+
+## via `om/set-query!`
+```
+;; ui
+(defui Component
+  static om/IQuery
+  (query [this]
+    [:foo/bar])
+  Object
+  (render [this]
+    (dom/button #js {:onClick #(do #(om/transact! this '[(button/clicked)])
+                                   ; we don't have `state` here so need to include relevant 
+                                   ; stuff in the component's query
+                                   ;
+                                   ; also we don't want to set query on `this` we probably want 
+                                   ; set the route query on the route component so have to pass 
+                                   ; callbacks all the way down to this component which is a pain
+                                   (if (valid? state) 
+                                     (om/set-query! this {:query {:route (route->query :foo)}}
+                                     (om/set-query! this {:query {:route (route->query :bar)}}))}))
+
+;; parsing
+(defmethod mutate 'button/clicked
+  [{:keys [state]} _ _]
+  (if (valid? @state)
+    {:action #(swap! state merge {:something :else})}
+    {:action #(swap! state merge {:another :thing})}))
+
+```
+
+Using `om/set-query!` is bad in this example because we are moving logic into components. Also now
+the UI has to be changed by calling both `om/transact!` and `om/set-query!`  instead of just 
+`om/transact!`.
+
+Would be curious if anyone has come up with a clean way to use `set-query!` to do routing.
--- src/om_router/core.cljs
+++ src/om_router/core.cljs
@@ -1,112 +1,100 @@
 (ns om-router.core
-  (:require [goog.dom :as gdom]
-            [om.next :as om :refer-macros [defui]]
-            [bidi.bidi :as bidi :refer [match-route]]
-            [om.dom :as dom]
-            [pushy.core :as pushy])
+  (:require
+    [goog.dom :as gdom]
+    [om.next :as om :refer-macros [defui]]
+    [bidi.bidi :as bidi :refer [match-route]]
+    [goog.string :as string]
+    [goog.string.format]
+    [om.dom :as dom]
+    [pushy.core :as pushy])
   (:require-macros
-            [cljs-log.core :as l :refer [debug info warn severe]]))
+    [cljs-log.core :refer [debug info warn severe]]))
 
 (enable-console-print!)
 
-;;; routing
+;; ============================================================================
+;; Routing
 
 (def routes ["/" {"" :index
                   "faq" :faq
-                  "contact" :contact
                   "about" :about}])
 
-(def route->url (partial bidi/path-for routes))
+(defn route->url
+  [route]
+  (bidi/path-for routes route))
 
-(defn parse-url [url]
-  (match-route routes url))
+(defn url->route [url]
+  (:handler (match-route routes url)))
 
-;;; parsing
+;; ============================================================================
+;; Parsing
 
 (defmulti mutate om/dispatch)
 
 (defmethod mutate 'route/update
-  [{:keys [state]} _ {:keys [new-route]}]
-  {:value {:keys [:route]}
-   :action (fn [] (swap! state assoc :route new-route))})
+  [{:keys [state]} _ {:keys [value]}]
+  {:value {:keys [:current-route]}
+   :action #(swap! state assoc :current-route value)})
 
 (defmulti read om/dispatch)
 
 (defmethod read :route
+  [{:keys [state query parser] :as env} _ _]
+  (let [{:keys [current-route]} @state]
+    {:value (parser (dissoc env :query)
+                    (get query current-route))}))
+
+(defmethod read :current-route
   [{:keys [state]} _ _]
-  {:value (get @state :route)})
+  {:value (get @state :current-route)})
 
-(defmethod read :page
-  [{:keys [query parser state] :as env} _ _]
-  (debug ":page query" query)
-  {:value (parser env query)})
+(defmethod read :default
+  [_ k _]
+  {:value (string/format "Reading: %s" (name k))})
 
-(defmethod read :page/home
-  [_ _ _]
-  (println "reading: home")
-  {:value "home"})
+;; ============================================================================
+;; Pages
 
-(defmethod read :page/contact
-  [_ _ _]
-  (println "reading: contact")
-  {:value "contact"})
-
-(defmethod read :page/faq
-  [_ _ _]
-  (println "reading: faq")
-  {:value "faq"})
-
-(defmethod read :page/about
-  [_ _ _]
-  (println "reading: about")
-  {:value "about"})
-
-;;; components
+(defn render-page
+  [this]
+  (dom/div nil (pr-str (om/props this))))
 
 (defui HomePage
   static om/IQuery
   (query [this]
     [:page/home])
   Object
-  (render [this]
-    (dom/div nil (:page/home (om/props this)))))
-
-(defui Contact
-  static om/IQuery
-  (query [this]
-    [:page/contact])
-  Object
-  (render [this]
-    (dom/div nil (:page/contact (om/props this)))))
+  (render [this] (render-page this)))
 
 (defui About
   static om/IQuery
   (query [this]
     [:page/about])
   Object
-  (render [this]
-    (dom/div nil (:page/about (om/props this)))))
+  (render [this] (render-page this)))
 
 (defui Faq
   static om/IQuery
   (query [this]
     [:page/faq])
   Object
-  (render [this]
-    (dom/div nil (:page/faq (om/props this)))))
+  (render [this] (render-page this)))
+
+;; ============================================================================
+;; Menu
 
 (defn menu-entry
-  [{:keys [route selected?] :as props}]
+  [{:keys [route selected?]}]
   (dom/li nil
     (dom/a (clj->js {:href (route->url route)})
       (if selected? (str (name route) " (selected)") (name route)))))
 
-;;; Mapping from route to components, queries, and factories
+;; ============================================================================
+;; Mapping from route to components, queries, and factories
 
 (def route->component
   {:index HomePage
    :faq Faq
-   :contact Contact
    :about About})
 
 (def route->factory
@@ -119,80 +107,47 @@
 
 (declare history)
 
-(defn update-query-from-route!
-  [this route]
-  (debug "Set-Params!")
-  (om/set-query! this {:params {:page (get route->query route)}}))
+(defn update-route
+  [root route]
+  (debug "update-route" (name route))
+  (om/transact! root `[(route/update {:value route}) :route]))
 
 (defui Root
-  static om/IQueryParams
-  (params [this]
-    {:page (:index route->query)})
   static om/IQuery
-  (query [this]
-    '[:route {:page ?page}])
-
+  (query [_]
+    "This is called the \"Total Query\" approach because `route->query` is the complete
+    query of the app."
+    [:current-route {:route route->query}])
   Object
-  (nav-handler
-    [this match]
-    "Sync: Browser -> OM"
-    (debug "Pushy caught a nav change" match)
-    (let [{route :handler} match]
-      (update-query-from-route! this route)
-      (om/transact! this `[(route/update {:new-route ~route})
-                           :route])))
-
-  (componentWillReceiveProps
-    [this props]
-    "Sync: OM -> Browser"
-    (let [[old-route new-route] [(:route (om/props this)) (:route props)]]
-      (debug "componentWillReceiveProps" old-route new-route)
-      (when (not= new-route old-route)
-        (update-query-from-route! this new-route)
-        (pushy/set-token! history (or (route->url new-route) "/unknown-route")))))
-
+  (componentDidUpdate [this _ _]
+    "Sync state -> url"
+    (let [{:keys [current-route]} (om/props this)]
+      (when (not= current-route (url->route js/location.pathname))
+        (pushy/set-token! history (route->url current-route)))))
   (componentDidMount [this]
-    (debug "App mounted")
-    (let [nav-fn #(.nav-handler this %)
-          pushy (pushy/pushy nav-fn parse-url)]
-      (debug "HISTORY/get-token" (pushy/get-token pushy))
-      (pushy/start! pushy)
-      (set! history pushy)))
-
-  (componentWillUnmount [this]
-    (debug "App unmounting")
+    "Sync url -> state"
+    (let [listener (pushy/pushy #(om/transact! this `[(route/update {:value ~%}) :route])
+                                url->route)]
+      (pushy/start! listener)
+      (set! history listener)))
+  (componentWillUnmount [_]
     (pushy/stop! history))
-
   (render [this]
-    (let [{:keys [route page]} (om/props this)
-          entries (vals (second routes))]
-
+    (let [{:keys [current-route route]} (om/props this)
+          factory (route->factory current-route)]
       (dom/div nil
-        ;; routing via pushy
-        (dom/div nil
-          (dom/p nil "change route via pushy and anchor tags")
-          (apply dom/ul nil
-                 (map (fn [cur-route]
-                        (menu-entry {:route cur-route :selected? (= cur-route route)}))
-                      entries)))
-
-        ;; routing in a transaction
-        (dom/div nil
-          (dom/p nil "change route via a transaction")
-          (dom/button #js {:onClick #(om/transact! this '[(route/update {:new-route :about})
-                                                         :route])}
-                      "goto about"))
-
-        ;; render the current page here
-        (dom/div nil
-          (dom/p nil "current page:")
-          ((route->factory route) page))))))
-
-(def parser (om/parser {:read read :mutate mutate}))
+        (dom/div nil "navigate via links:")
+        (apply dom/div nil (map #(menu-entry {:route % :selected? (= % current-route)})
+                                (keys route->component)))
+        (dom/div nil "navigate via transactions:")
+        (dom/button #js {:onClick #(om/transact! this '[(route/update {:value :about}) :route])}
+                    "Goto about page")
+        (dom/div nil "current page:")
+        (factory route)))))
 
 (def reconciler
   (om/reconciler
-    {:state (atom {:route :index})
-     :parser parser}))
+    {:state (atom {:current-route (url->route js/location.pathname)})
+     :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler Root (gdom/getElement "app"))
